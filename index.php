6f206eb8c45d86544f31414a41d79d235c768e03
lucascuevas <lucascuevas@live.com.ar>
2015-07-13 00:09:56 -0300

    imagenes

--- index.php
+++ index.php
@@ -67,20 +67,14 @@
 			<!-- Wrapper for slides -->
 			<div class="carousel-inner" role="listbox">
 				<div class="item active">
-					<img src="img/kriptonita.jpg" alt="Chania" width="100%" height="100%">
+					<img src="img/bestnid1.jpg" alt="Chania" width="100%" height="100%">
 				</div>
 
 				<div class="item">
-					<img src="img/llama.jpg" alt="llama.jpg" width="100%" height="100%">
+					<img src="img/bestnid2.jpg" alt="llama.jpg" width="100%" height="100%">
 				</div>
 
-				<div class="item">
-					<img src="img/marco.jpg" alt="marco.jpg" width="100%" height="100%">
-				</div>
-
-				<div class="item">
-					<img src="img/silla.jpg" alt="silla.jpg" width="100%" height="100%">
-				</div>
+				
 			</div>
 		<!-- Left and right controls -->
 			<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
